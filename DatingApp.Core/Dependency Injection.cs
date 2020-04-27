a310b759247b0cc36b24cf22d6488117f211869f
vimoru <vimoru@pajuno.com>
2020-04-27 05:26:54 +0100

    Inital Project Structure

---
+++ DatingApp.Core/Dependency Injection.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DatingApp.Core
+{
+    public class DependecyInjection
+    {
+    }
+}
---
+++ Shared.Infrastructure/Dependency Injection.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shared.Infrastructure
+{
+    public class DependecyInjection
+    {
+    }
+}
